9c27c8116f394826f798153dfeaf5cdf5f3cf9af
Andrew Knoblach <amknoblach@gmail.com>
2022-01-21 14:41:49 -0700

    HKDF works and passes SHA256 specs

--- src/hkdf.cr
+++ src/hkdf.cr
@@ -10,32 +10,35 @@ module Hkdf
   
   def self.hkdf(ikm, length=32 ,salt : Bytes=Bytes.empty, info : Bytes=Bytes.empty, algo=OpenSSL::Algorithm::SHA256)
     #prk = psuedorandom key
+    #Extract step
     prk = hmac(algo,salt, ikm)
+
     hash_len = prk.size 
     #Check for proper hash lenght
-    if length > 255 * hash_len
-      raise "Length cannot be larger than 255 * Hash Length"
-    end
+    raise "Length cannot be larger than 255 * Hash Length" if length > 255 * hash_len
 
     n = (length/hash_len).ceil.to_i
     t = Array(Bytes).new
     t.push(Bytes.empty)
+
+    #Expand
     (1..n).each do |i|
       buf = IO::Memory.new
       buf.write(t[i-1])
       buf.write(info)
-      buf.write(i.chr)
-      okm.push(hmac(algo, prk, buf))
+      buf.write_byte(i.to_u8)
+      t.push(hmac(algo, prk, buf))
     end
 
     okm = IO::Memory.new
     t.each do |b|
-      okm.write_bytes(b)
+      okm.write(b)
     end
 
-    return okm.to_slice
-
+    output = Bytes.new(length)
+    okm.rewind
+    okm.read(output)
+    return output
+    #okm.to_slice[0,length]
   end
-
-
 end
